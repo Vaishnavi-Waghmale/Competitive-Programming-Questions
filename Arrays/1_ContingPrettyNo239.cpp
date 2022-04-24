cefe2cf0c5ef48f0c5a70ea1003c04e16a7e872c
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-04-24 12:16:17 +0530

    Create 1_ContingPrettyNo239.cpp

---
+++ Arrays/1_ContingPrettyNo239.cpp
@@ -0,0 +1,28 @@
+//Vaishnavi-Waghmale
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    
+    cin>>t;
+    
+    while(t--){
+        int l, r;
+        int count=0;
+        cin>>l>>r;
+        
+        
+        for (int i = l; i <=r ; i++)
+        {
+            if (i%10==2|i%10==3|i%10==9)
+            {
+                count++;
+            } 
+        }
+    
+        cout<<count<<"\n";
+    }
+    
+	return 0;
+}
