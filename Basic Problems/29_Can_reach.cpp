17b866406d66e004c68318a5ea69c18283ed418b
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-05-02 18:11:22 +0530

    Add files via upload

---
+++ Basic Problems/29_Can_reach.cpp
@@ -0,0 +1,20 @@
+//Vaishnavi-Waghmale
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y, k;
+        cin>>x>>y>>k;
+        
+        if(x%k==0 && y%k==0){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+	return 0;
+}
