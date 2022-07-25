95f77eee69904e3717397faf8265ad3164c402ba
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-25 21:30:42 +0530

    Create 34_chef_in_his_office.cpp
    new problem

---
+++ Basic Problems/34_chef_in_his_office.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int x,y;
+	    cin>>x>>y;
+	    int t;
+	    t=4*x;
+	    
+	    cout<<t+y<<endl;
+	}
+	return 0;
+}
