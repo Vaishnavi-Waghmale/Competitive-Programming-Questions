7c498ae8577881b93a37706df758d810bfd73021
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-25 18:35:48 +0530

    Create Shopping_Change.cpp
    New Problem

---
+++ Basic Problems/Shopping_Change.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	for(int i=0; i<=T; i++){
+	    int X;
+	    cin>>X;
+	    
+	    int ret;
+	    ret= 100-X;
+	    cout<<ret<<endl;
+	   
+	}
+	return 0;
+}
