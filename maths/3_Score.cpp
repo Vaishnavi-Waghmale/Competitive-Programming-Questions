117c3bbbe0c2ed94d26ac88e19e120142498c0f9
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-26 22:00:32 +0530

    Create 3_Score.cpp
    New problem

---
+++ maths/3_Score.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,n;
+        cin>>x>>n;
+        
+        int a,b;
+        
+        a= x/10;
+        
+        b=n*a;
+        
+        cout<<b<<endl;
+    }
+	return 0;
+}
