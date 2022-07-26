9412c98d3e73b5fff91c6b5daf39ca398b8bf447
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-26 22:14:57 +0530

    Create 5_Total_prize_money.cpp
    New problem

---
+++ maths/5_Total_prize_money.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int x,y;
+        cin>>x>>y;
+        int ans;
+        ans= x*10 + y*90;
+        
+        cout<<ans<<endl;
+    }
+	return 0;
+}
