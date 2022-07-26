3f79592f995674906b0fda7b60a514dcc33bf322
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-26 22:10:34 +0530

    Create 4_1st_problem.cpp
    New problem

---
+++ maths/4_1st_problem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+        int n,a,b;
+        cin>>n>>a>>b;
+        
+        int p,q;
+        p= n-a;
+        q=p-b;
+        cout<<p<<" "<<q<<endl;
+    
+	return 0;
+}
