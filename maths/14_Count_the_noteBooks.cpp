cab86aac589de94479ed8d922c0acd3841e86d92
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-27 18:40:16 +0530

    Create 14_Count_the_noteBooks.cpp
    New problem

---
+++ maths/14_Count_the_noteBooks.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans;
+        ans= (n*1000)/100;
+        
+        cout<<ans<<endl;
+    }
+	return 0;
+}
