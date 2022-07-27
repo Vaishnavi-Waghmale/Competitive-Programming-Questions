5d039cc1d1945a3cad4d8d6ae90bb5487859b26f
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-27 18:33:23 +0530

    Create 12_Fill_the_bucket.cpp
    New problem

---
+++ maths/12_Fill_the_bucket.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int k,x;
+        cin>>k>>x;
+        int ans;
+        ans= k-x;
+        cout<<ans<<endl;
+    }
+	return 0;
+}
