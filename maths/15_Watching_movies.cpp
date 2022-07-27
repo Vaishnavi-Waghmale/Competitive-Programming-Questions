44e2d0792d418206fc16f23eadefa167d8dbb21d
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-27 18:46:40 +0530

    Create 15_Watching_movies.cpp
    New problem

---
+++ maths/15_Watching_movies.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    
+    int New;
+    New=y/2;
+    int ans;
+    ans= New + (x-y);
+    cout<<ans<<endl;
+    
+	return 0;
+}
