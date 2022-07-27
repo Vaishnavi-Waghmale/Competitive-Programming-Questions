e751f9f0fab4d0b3b54c019eee00d9284fb20921
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-27 17:56:50 +0530

    Create 8_Wating_time.cpp
    New problem

---
+++ maths/8_Wating_time.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int k,x;
+        cin>>k>>x;
+        
+        int d,ans;
+        d=k*7;
+        ans=d-x;
+        
+        cout<<ans<<endl;
+    }
+	return 0;
+}
