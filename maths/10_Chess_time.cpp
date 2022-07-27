6be5cd351a71832b76db8b3128d69a7b3eaf239a
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-27 18:22:10 +0530

    Create 10_Chess_time.cpp
    New problem

---
+++ maths/10_Chess_time.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int min =n*60;
+        int ans;
+        ans=min/20;
+        cout<<ans<<endl;
+    }
+	return 0;
+}
