b0d6d8ad9485d0e4f7196440974f665e7569b6de
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-27 18:28:18 +0530

    Create 11_Apples&Oranges.cpp
    New problem

---
+++ maths/11_Apples&Oranges.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    int a,b;
+    cin>>a>>b;
+    int c;
+    c=a+b;
+    if(c<=x){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+	return 0;
+}
