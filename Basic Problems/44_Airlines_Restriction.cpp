e192926fb5dc796125125c32ac6475d8c0d00548
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-07-30 22:09:23 +0530

    Create 44_Airlines_Restriction.cpp
    New problem

---
+++ Basic Problems/44_Airlines_Restriction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d,e;
+        cin>>a>>b>>c>>d>>e;
+        if(a+b<=d && c<=e){
+            cout<<"Yes"<<endl;
+        }
+        else if(b+c<=d && a<=e){
+            cout<<"Yes"<<endl;
+        }
+        else if(a+c<=d && b<=e){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+        
+    }
+	return 0;
+}
