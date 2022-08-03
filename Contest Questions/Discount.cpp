c0eedbe1f7127bb3958489cf803cd2db8477acb5
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-03 22:28:37 +0530

    Create Discount.cpp
    New problem

---
+++ Contest Questions/Discount.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    cout<<100-x<<endl;
+	}
+	return 0;
+}
