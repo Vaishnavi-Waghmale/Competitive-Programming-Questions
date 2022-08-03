029e3b2230f6c7cebc8da982ded7225b1fd95644
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-03 19:34:32 +0530

    Create 56_Packaging_cupcakes.cpp
    New problem

---
+++ Basic Problems/56_Packaging_cupcakes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long int n;
+	    cin>>n;
+	    long long int k = (n/2)+1;
+	    cout<<k<<endl;
+	}
+	return 0;
+}
