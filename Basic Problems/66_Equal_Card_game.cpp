3259da0e31be3a3a2c6b1cf8b2196a33b79f2828
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-09 18:02:44 +0530

    Create 66_Equal_Card_game.cpp
    New problem

---
+++ Basic Problems/66_Equal_Card_game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int k;
+        cin>>k;
+        int ans = 52%k;
+        cout<<ans<<endl;
+    }
+	return 0;
+}
