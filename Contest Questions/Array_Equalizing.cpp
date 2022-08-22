dee04e4d06a0690c3f1dec19af409e43bd2bed69
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-22 22:35:40 +0530

    New problem

---
+++ Contest Questions/Array_Equalizing.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll a, maxfreq = 0, m;
+        map<ll, ll>freq;
+        for(ll i=0; i<n; i++){
+            cin>>a;
+            freq[a]++;
+            maxfreq= max(maxfreq, freq[a]);
+        }
+        m = n/2;
+        if(n%2) m++;
+        if(maxfreq <=m){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+	return 0;
+}
