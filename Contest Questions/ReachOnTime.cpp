383d66a289f845592d2a1bdd68881e3ec0fc838b
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-10-23 19:44:28 +0530

    New problem

---
+++ Contest Questions/ReachOnTime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>=30){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
