eb7847caec4f8826ed0ea5fd2eebd532c0c17c4f
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-10-23 19:47:55 +0530

    New problem

---
+++ Contest Questions/WhoIsTaller.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(x>y){
+	        cout<<"A"<<endl;
+	    }
+	    else{
+	        cout<<"B"<<endl;
+	    }
+	}
+	return 0;
+}
