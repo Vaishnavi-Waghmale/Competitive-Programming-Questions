6b46efe719dfb9699a52209b4c28321f3758f4b9
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-10-25 12:43:57 +0530

    New problem

---
+++ Basic Problems/Audible_Ranges.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        
+        if(67<=x && x<=45000){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
