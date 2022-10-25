85c959c9efed7bd4bde0ba1267f638ec1fa54ba9
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-10-25 12:49:20 +0530

    New problem

---
+++ Basic Problems/Reach_The_Target.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int req;
+	    req = x-y;
+	    cout<<req<<endl;
+	}
+	return 0;
+}
