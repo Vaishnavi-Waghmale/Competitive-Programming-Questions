483f1111470b975bb7580da66b9ac15d89f944bb
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-10-29 13:48:43 +0530

    New problem

---
+++ 1* to 2* practise/Distinct Pair Sums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int l,r;
+	    cin>>l>>r;
+	    int count=r-l+1+r-l;
+	    
+	    cout<<count<<endl;
+	}
+	return 0;
+}
