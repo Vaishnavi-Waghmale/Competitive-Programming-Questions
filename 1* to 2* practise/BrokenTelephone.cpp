1acd1a9309ae6f5c637ad3e56f4f654d10e2191e
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-11-08 14:42:47 +0530

    New problem

---
+++ 1* to 2* practise/BrokenTelephone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    int count = 0;
+	    for(int i=0; i<n; i++){
+	        cin>>arr[i];
+	        
+	        if(arr[0]!=arr[i]){
+	            count++;
+	        }
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
