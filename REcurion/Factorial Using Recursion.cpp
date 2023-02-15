4b437c0338104df3175cddd2a63f70aa7cab9ba1
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2023-02-15 10:50:25 +0530

    New problem

---
+++ REcurion/Factorial Using Recursion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+    int divisions = n * factorial(n-1);
+    
+    return divisions;
+}
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	
+	int ans = factorial(n);
+	
+	cout<<ans<<endl;
+	return 0;
+}
