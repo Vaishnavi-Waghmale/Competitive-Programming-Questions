1a130cc3d8c8f640f406ba0041a9e8fcf2275dd9
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2023-02-20 20:57:02 +0530

    Update Binary search using recursion.cpp

--- Recursion/Binary search using recursion.cpp
+++ Recursion/Binary search using recursion.cpp
@@ -1,9 +1,14 @@
 bool binarySearch(int *arr, int s, int e, int k){
   
+  
   if(s>e)
       return false;
   
   int mid = s+(e-s)/2;
+  
+  if(arr[mid]==k)
+    return true
+  
   
   if(arr[mid] < k){
     return binarySearch(arr, mid+1, e, k);
