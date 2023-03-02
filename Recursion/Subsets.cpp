7fac3a24605d728221ea2fb1ca8a9d1b381d1338
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2023-03-02 19:43:24 +0530

    New problem

---
+++ Recursion/Subsets.cpp
@@ -0,0 +1,30 @@
+//Subsets - Power set - set of allm subsets of given set
+
+class Solution {
+public:
+    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans){
+
+        //base case
+        if(index >= nums.size()){
+            ans.push_back(output);
+            return;
+        }
+
+        //exclude
+        solve(nums, output, index+1, ans);
+
+        int element;
+        element = nums[index];
+        output.push_back(element);
+        solve(nums, output, index+1, ans);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index  = 0;
+
+        solve(nums, output, index, ans);
+        return ans;
+    }
+};
