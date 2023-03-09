edf312521843d4528e40f9b8b5e8bb9de97fff48
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2023-03-09 14:30:57 +0530

    Update Rat in a maze.cpp

--- Recursion/Rat in a maze.cpp
+++ Recursion/Rat in a maze.cpp
@@ -1,2 +1,17 @@
 
 //Rat in a maze problem
+
+vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+        vector<string> ans;
+        
+        if(m[0][0] == 0){
+            return ans;
+        }
+        
+        int startx = 0;
+        int starty = 0;
+        
+        
+        
+    }
