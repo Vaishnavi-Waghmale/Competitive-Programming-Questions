6c96fc726aa1e3fa4d9c66fd16b4ededf8aac946
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2023-03-11 22:33:44 +0530

    Update Rat in a maze.cpp

--- Recursion/Rat in a maze.cpp
+++ Recursion/Rat in a maze.cpp
@@ -1,7 +1,73 @@
 
 //Rat in a maze problem
 
-vector<string> findPath(vector<vector<int>> &m, int n) {
+class Solution{
+    public:
+    bool isSafe(int x, int y, int n, vector<vector<int>>visited, vector<vector<int>>& m){
+        if((x>=0 && x<n) && (y>=0 && y<n) && visited[x][y]==0 && m[x][y]==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    void solve(vector<vector<int>> &m, int n, vector<string> &ans, int x, int y, vector<vector<int>>visited, string path){
+        if(x==n-1 && y==n-1){
+            ans.push_back(path);
+            return;
+        }
+        
+        visited[x][y]=1;
+        //down
+        int newx = x+1;
+        int newy = y;
+        
+        if(isSafe(newx, newy, n, visited, m)){
+            path.push_back('D');
+            
+            solve(m, n, ans, newx, newy, visited, path);
+            
+            path.pop_back();
+        }
+        
+        //left
+        newx = x;
+        newy = y+1;
+        
+        if(isSafe(newx, newy, n, visited, m)){
+            path.push_back('L');
+            
+            solve(m, n, ans, newx, newy, visited, path);
+            
+            path.pop_back();
+        }
+        
+        //right
+        newx = x;
+        newy = y-1;
+        
+        if(isSafe(newx, newy, n, visited, m)){
+            path.push_back('R');
+            
+            solve(m, n, ans, newx, newy, visited, path);
+            
+            path.pop_back();
+        }
+        
+        //up
+        newx = x-1;
+        newy = y;
+        
+        if(isSafe(newx, newy, n, visited, m)){
+            path.push_back('U');
+            
+            solve(m, n, ans, newx, newy, visited, path);
+            
+            path.pop_back();
+        }
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
         vector<string> ans;
         
@@ -9,9 +75,24 @@ vector<string> findPath(vector<vector<int>> &m, int n) {
             return ans;
         }
         
-        int startx = 0;
-        int starty = 0;
+        int srcx = 0;
+        int srcy = 0;
         
+        vector<vector<int>>visited;
         
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                visited[i][j]=0;
+            }
+        }
+        
+        string path = "";
+        
+        solve(m, n, ans, srcx, srcy, visited, path);
+        
+        sort(ans.begin(), ans.end());
+        
+        return ans;
         
     }
+};
