4ee736ed671be07945f9cc712db9bf3a32bc741c
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2023-03-19 20:31:29 +0530

    Create Chef and dolls.cpp

---
+++ Basic Problems/Chef and dolls.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	   int n,ans=0;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    sort(arr,arr+n);
+	    for(int i=0;i<n;i=i+2){
+	        if(arr[i]!=arr[i+1]) {
+	            cout<<arr[i]<<endl;
+	            break;
+	    }
+	    }
+	}
+	return 0;
+}
